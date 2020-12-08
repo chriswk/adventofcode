860ca75de87f270f87c4b63a98eb0711b0b07c35
Christopher Kolstad <git@chriswk.no>
2020-12-08 10:52:21 +0100

    Solved 2020.12.08 part 2

--- src/main/kotlin/com/chriswk/aoc/advent2020/Day8.kt
+++ src/main/kotlin/com/chriswk/aoc/advent2020/Day8.kt
@@ -26,31 +26,64 @@ class Day8: AdventDay(2020, 8) {
         return input.map { parseInstruction(it) }.toTypedArray()
     }
 
-    fun runProgram(program: Array<Instruction>): Int {
+    fun runProgram(program: Array<Instruction>): Pair<Int, Boolean> {
         val seen = mutableSetOf<Int>()
         var ip = 0
         var acc = 0
-        while(!seen.contains(ip)) {
+        val size = program.size
+        var exceptionThrown = false
+        while(!seen.contains(ip) && ip < size && !exceptionThrown) {
+            exceptionThrown = false
             seen.add(ip)
-            val ins = program[ip]
-            when(ins.op) {
-                Op.NOP -> ip += 1
-                Op.ACC -> {
-                    acc += ins.value
-                    ip += 1
+            if (ip < 0 || ip > size) {
+                exceptionThrown = true
+            } else {
+                val ins = program[ip]
+                when (ins.op) {
+                    Op.NOP -> ip += 1
+                    Op.ACC -> {
+                        acc += ins.value
+                        ip += 1
+                    }
+                    Op.JMP -> ip += ins.value
                 }
-                Op.JMP -> ip += ins.value
             }
         }
-        return acc
+        return acc to (!exceptionThrown && ip >= size)
     }
 
     fun part1(): Int {
-        return runProgram(buildInstructions(inputAsLines))
+        return runProgram(buildInstructions(inputAsLines)).first
+    }
+
+    fun toggle(instruction: Instruction): Instruction {
+        return when(instruction.op) {
+            Op.JMP -> instruction.copy(op = Op.NOP)
+            Op.NOP -> instruction.copy(op = Op.JMP)
+            Op.ACC -> instruction.copy(op = Op.ACC)
+        }
+    }
+
+    fun toggleOps(program: Array<Instruction>): Int {
+        return program.asSequence().mapIndexed { idx, instruction ->
+            val toggle = when (instruction.op) {
+                Op.NOP -> instruction.value != 0
+                Op.JMP -> true
+                Op.ACC -> false
+            }
+            if (toggle) {
+                val c = program.copyOf()
+                c[idx] = toggle(instruction)
+                runProgram(c)
+            } else {
+                (0 to false)
+            }
+        }.first { it.second }.first
     }
 
     fun part2(): Int {
-        return 0
+        val program = buildInstructions(inputAsLines)
+        return toggleOps(program)
     }
 
     data class Instruction(val op: Op, val value: Int)
--- src/test/kotlin/com/chriswk/aoc/advent2020/Day8Test.kt
+++ src/test/kotlin/com/chriswk/aoc/advent2020/Day8Test.kt
@@ -4,10 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class Day8Test {
-
-    @Test
-    fun testProgramFindsLoop() {
-        val input = """
+    val input = """
             nop +0
             acc +1
             jmp +4
@@ -18,13 +15,28 @@ class Day8Test {
             jmp -4
             acc +6
         """.trimIndent().lines()
+    @Test
+    fun testProgramFindsLoop() {
+
         val day = Day8()
         val program = day.buildInstructions(input)
-        assertThat(day.runProgram(program)).isEqualTo(5)
+        assertThat(day.runProgram(program).first).isEqualTo(5)
     }
 
     @Test
     fun part1() {
         assertThat(Day8().part1()).isEqualTo(1941)
+    }
+
+    @Test
+    fun testProgramManagesToUnwrapLoop() {
+        val day = Day8()
+        val program = day.buildInstructions(input)
+        assertThat(day.toggleOps(program)).isEqualTo(8)
+    }
+
+    @Test
+    fun part2() {
+        assertThat(Day8().part2()).isEqualTo(2096)
     }
 }
